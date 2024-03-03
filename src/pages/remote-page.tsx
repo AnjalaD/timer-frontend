29ad76ce9268ecd6b4e3c992b5b0e240ce9f4c06
AnjalaD <anjaladilhara@gmail.com>
2024-03-03 21:20:36 +0530

    Improve remote page ui

--- src/pages/remote-page.tsx
+++ src/pages/remote-page.tsx
@@ -1,7 +1,9 @@
 import { useParams } from "react-router-dom";
 import { useRemoteStore } from "../components/remote/remote-store";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../components/ui/button";
+import { PauseIcon, PlayIcon } from "@radix-ui/react-icons";
+import { cn } from "../util/cn";
 
 export const RemotePage = () => {
   const { roomId } = useParams<{ roomId: string }>();
@@ -13,6 +15,16 @@ export const RemotePage = () => {
   const pause = useRemoteStore((state) => state.pause);
   const disconnect = useRemoteStore((state) => state.disconnect);
 
+  const [light, setLight] = useState(false);
+
+  const withLight = (fn: () => void) => () => {
+    setLight(true);
+    setTimeout(() => {
+      setLight(false);
+    }, 200);
+    fn();
+  };
+
   useEffect(() => {
     init(roomId!);
     return () => {
@@ -23,17 +35,36 @@ export const RemotePage = () => {
   return (
     <div className="bg-gray-900 text-white">
       <div className="min-h-screen flex items-center justify-center">
-        <div className="max-w-80 rounded-2xl bg-black p-4">
-          <p>Timer ID: {roomId}</p>
-          {!isReady ? <p>Connecting...</p> : <p>Connected!</p>}
+        <div className="relative max-w-80 rounded-t-[100px] rounded-b-[200px] bg-black p-16 pb-32 shadow-xl">
+          <div className="absolute -top-2 left-1/2 transform -translate-x-1/2">
+            <div
+              className={cn(
+                "w-8 h-3 rounded-t-[100px] rounded-b-md",
+                light ? "bg-green-500" : "bg-black"
+              )}
+            />
+          </div>
 
           <div className="flex flex-col items-center gap-8">
-            <Button disabled={!isReady} onClick={start}>
-              Start
-            </Button>
+            <div className="flex flex-col gap-4 rounded-xl bg-gray-600 p-4 text-center mb-8">
+              <div className="text-xs">Timer ID: {roomId}</div>
+              {!isReady ? <div>Connecting...</div> : <div>Connected!</div>}
+            </div>
 
-            <Button disabled={!isReady} variant="outline" onClick={pause}>
-              Pause
+            <Button
+              className="rounded-full p-8 text-lg"
+              disabled={!isReady}
+              onClick={withLight(start)}
+            >
+              <PlayIcon className="mr-3 h-6 w-6" /> Start
+            </Button>
+            <Button
+              className="rounded-full p-8 text-lg"
+              disabled={!isReady}
+              variant="outline"
+              onClick={withLight(pause)}
+            >
+              <PauseIcon className="mr-3 h-6 w-6" /> Pause
             </Button>
           </div>
         </div>
