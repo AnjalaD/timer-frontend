61c970d3a2f5c930097e6c319326c97f3fce9860
AnjalaD <anjaladilhara@gmail.com>
2024-03-03 18:34:08 +0530

    Remove home page

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
     {
       path: "/",
       element: <HomePage />,
+      
     },
     {
       path: "/timer",
--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,10 +1,5 @@
-import { Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 export const HomePage = () => {
-  return (
-    <div className="min-h-100vh">
-      <h1>Home</h1>
-      <Link to="/timer">Timer</Link>
-    </div>
-  );
+  return <Navigate to="/timer" />;
 };
